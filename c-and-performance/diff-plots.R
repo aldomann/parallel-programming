4b8321b41e157b3c41a6380780b1a8d191858fd6
Alfredo Hernández <aldomann.designs@gmail.com>
2017-10-24 13:51:53 +0200

    Improved version of the performance tests

--- c-and-performance/diff-plots.R
+++ c-and-performance/diff-plots.R
@@ -7,15 +7,17 @@ library(tidyverse)
 data <- read_csv("c-and-performance/perf-res-diff.csv")
 
 data <- data %>%
-	mutate(op = nx * ny * nz * (3 + 100)) %>%
+	mutate(op = nx * ny * nz * (3 + iter)) %>%
 	mutate(size = nx * ny * nz) %>%
 	mutate(ipc = inst/cycl) %>%
 	mutate(clck = cycl/time/10^9) %>%
 	mutate(opps = op/time/10^6) %>%
 	mutate(type = "1")
 
-data2 <- data %>%
-	mutate(op = nx * ny * nz * (3 + 100)) %>%
+data2 <- read_csv("c-and-performance/perf-res-diff.csv")
+
+data2 <- data2 %>%
+	mutate(op = nx * ny * nz * (3 + iter)) %>%
 	mutate(size = nx * ny * nz) %>%
 	mutate(ipc = inst/cycl) %>%
 	mutate(clck = cycl/time/10^9) %>%
@@ -24,5 +26,5 @@ data2 <- data %>%
 
 new.data <- rbind(data, data2)
 
-ggplot(new.data, aes(x = size, y = opps, fill = type))+
+ggplot(data, aes(x = size, y = opps, fill = type))+
 	geom_bar(stat = "identity", position = "dodge")
--- c-and-performance/do-perf-server.sh
+++ c-and-performance/do-perf-server.sh
@@ -3,18 +3,20 @@ NX=$1
 NY=$2
 NZ=$3
 
-perf stat -o test.txt -e cycles,instructions ./diffrun $NX $NY $NZ
-
 if [ ! -f perf-res-diff.csv ]; then
 	touch perf-res-diff.csv
-	echo "NX,NY,NZ,cycles,instructions,time" >> perf-res-diff.csv
+	echo "iter,nx,ny,nz,cycl,inst,time" >> perf-res-diff.csv
 fi
 
-echo -n $(grep "stats" test.txt | awk '{print substr($0,32) }' | grep -o -E '[0-9]+'| sed ':a;N;$!ba;s/\n/,/g') >> perf-res-diff.csv
+echo -n $(perf stat -o perf-out.txt -e cycles,instructions ./diffrun $NX $NY $NZ | cut -d ' ' -f8) >> perf-res-diff.csv
 echo -n "," >> perf-res-diff.csv
-echo -n $(grep "cycles" test.txt | awk '{print substr($1,0,19) }' | sed 's/\.$//' | sed 's/\.//g') >> perf-res-diff.csv
+echo -n $(grep "stats" perf-out.txt | awk '{print substr($0,32) }' | grep -o -E '[0-9]+'| sed ':a;N;$!ba;s/\n/,/g') >> perf-res-diff.csv
 echo -n "," >> perf-res-diff.csv
-echo -n $(grep "instructions" test.txt | awk '{print substr($1,0,19) }' | sed 's/\.//g') >> perf-res-diff.csv
+echo -n $(grep "cycles" perf-out.txt | awk '{print substr($1,0,19) }' | sed 's/\.//g') >> perf-res-diff.csv
 echo -n "," >> perf-res-diff.csv
-echo -n $(grep "time elapsed" test.txt | awk '{print substr($1,0,19) }' | sed 's/,/\./g') >> perf-res-diff.csv
+echo -n $(grep "instructions" perf-out.txt | awk '{print substr($1,0,19) }' | sed 's/\.//g') >> perf-res-diff.csv
+echo -n "," >> perf-res-diff.csv
+echo -n $(grep "time elapsed" perf-out.txt | awk '{print substr($1,0,19) }' | sed 's/,/\./g') >> perf-res-diff.csv
 echo >> perf-res-diff.csv
+
+rm perf-out.txt
