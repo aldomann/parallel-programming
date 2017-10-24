459eae649e98d6afe7fb29353ca6eb2128b1ae04
Alfredo Hernández <aldomann.designs@gmail.com>
2017-10-24 18:26:15 +0200

    x axis is formatted properly

--- c-and-performance/diff-plots.R
+++ c-and-performance/diff-plots.R
@@ -4,27 +4,43 @@
 
 library(tidyverse)
 
-data <- read_csv("c-and-performance/perf-res-diff.csv")
+# Functions definitions ------------------------------------
 
-data <- data %>%
-	mutate(op = nx * ny * nz * (3 + iter)) %>%
-	mutate(size = nx * ny * nz) %>%
-	mutate(ipc = inst/cycl) %>%
-	mutate(clck = cycl/time/10^9) %>%
-	mutate(opps = op/time/10^6) %>%
-	mutate(type = "1")
+# Rounded version of f2si from sitools library
+f2si2 <- function (number) {
+	lut <- c(1, 1000, 1e+06, 1e+09)
+	pre <- c("", "k", "M", "G")
+	ix <- findInterval(number, lut)
 
-data2 <- read_csv("c-and-performance/perf-res-diff.csv")
+	if (ix > 0 && ix < length(lut) && lut[ix] != 1) {
+		sistring <- paste(round(number/lut[ix]), pre[ix])
+	}
+	else {
+		sistring <- as.character(number)
+	}
+	return(sistring)
+}
 
-data2 <- data2 %>%
-	mutate(op = nx * ny * nz * (3 + iter)) %>%
-	mutate(size = nx * ny * nz) %>%
-	mutate(ipc = inst/cycl) %>%
-	mutate(clck = cycl/time/10^9) %>%
-	mutate(opps = op/time/10^6) %>%
-	mutate(type = "2")
+read_results <- function(file, type) {
+	data <- read_csv(file)
+	data <- data %>%
+		mutate(op = nx * ny * nz * (3 + iter)) %>%
+		mutate(size = nx * ny * nz) %>%
+		mutate(ipc = inst/cycl) %>%
+		mutate(clck = cycl/time/10^9) %>%
+		mutate(opps = op/time/10^6) %>%
+		mutate(type = type)
+	return(data.frame(data))
+}
 
-new.data <- rbind(data, data2)
+# Read data and create plots -------------------------------
 
-ggplot(data, aes(x = size, y = opps, fill = type))+
-	geom_bar(stat = "identity", position = "dodge")
+data1 <- read_results("c-and-performance/perf-res-diff.csv", "base")
+data2 <- read_results("c-and-performance/perf-res-diff.csv", "optimised")
+
+new.data <- rbind(data1, data2)
+
+ggplot(new.data, aes(x = reorder(f2si2(size), size), y = opps, fill = type))+
+	geom_bar(stat = "identity", position = "dodge") +
+	labs(x = "Size of the problem", y = "Performance (Giga SAXPYs/second)")
+
